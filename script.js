a7475ddaf65d46c1d7c255e41e8268ccd33c0482
SarvaniTatori <sarvanitatori04@gmail.com>
2026-07-09 11:28:06 +0530

    BACK to OLD

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("AWS CI/CD Project");
-
-const button = document.querySelector("button");
-
-button.addEventListener("click", () => {
-
-alert("This application was deployed automatically using GitHub Actions and AWS!");
-
-});
