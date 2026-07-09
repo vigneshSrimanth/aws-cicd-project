55b233ff8ef052f56841630b62fe197f29ffb9bd
SarvaniTatori <sarvanitatori04@gmail.com>
2026-07-09 11:17:14 +0530

    changed page

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("AWS CI/CD Project");
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+
+alert("This application was deployed automatically using GitHub Actions and AWS!");
+
+});
